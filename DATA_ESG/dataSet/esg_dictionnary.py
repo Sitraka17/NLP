f9740e6a5500f15b968964d3f5ee73f1107b376d
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-16 18:52:01 +0100

    esg_dic

---
+++ DATA_ESG/dataSet/esg_dictionnary.py
@@ -0,0 +1,22 @@
+esg_dict = {
+    "Environmental": {
+        "Carbon Emissions": "The amount of greenhouse gases emitted by a company",
+        "Renewable Energy": "The proportion of a company's energy consumption that comes from renewable sources",
+        "Water Management": "How a company manages and conserves water resources",
+        "Waste Management": "How a company disposes of its waste and manages its environmental impact"
+    },
+    "Social": {
+        "Diversity and Inclusion": "The extent to which a company promotes diversity and inclusion within its workforce",
+        "Labor Standards": "The company's adherence to fair labor practices and human rights standards",
+        "Community Engagement": "The company's involvement in and support of local communities",
+        "Customer Satisfaction": "The extent to which a company meets its customers' needs and expectations"
+    },
+    "Governance": {
+        "Board Diversity": "The diversity of a company's board of directors",
+        "Executive Compensation": "The structure and amount of compensation for senior executives",
+        "Shareholder Rights": "The extent to which shareholders have a say in the company's decisions",
+        "Risk Management": "How a company identifies and manages its risks"
+    }
+}
+
+print(esg_dict)
